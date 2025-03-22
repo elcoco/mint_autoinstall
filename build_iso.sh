fcc13a30180045720689cdb253f98dd54d72ace9
eco <spambaconspam@protonmail.com>
2025-03-22 14:42:02 +0100

    added cfg option to script

--- build_iso.sh
+++ build_iso.sh
@@ -6,7 +6,7 @@ GRUB_CFG=$(pwd)/grub.cfg
 TMP_DIR=/tmp/build_iso
 
 usage() {
-    echo "build_iso.sh -i <INPUT_ISO> -o <OUTPUT_ISO>"
+    echo "build_iso.sh -i <INPUT_ISO> -o <OUTPUT_ISO> -c <REPO_DIR>"
 }
 
 die() {
@@ -30,7 +30,7 @@ cleanup() {
     fi
 }
 
-while getopts ":i:o:h" c; do
+while getopts ":i:o:c:h" c; do
     case $c in
         i) 
             ISO_IN="$OPTARG"
@@ -38,9 +38,12 @@ while getopts ":i:o:h" c; do
         o) 
             ISO_OUT="$OPTARG"
             ;;
+        c) 
+            CFG_DIR="$OPTARG"
+            ;;
         :)
-            echo "option -$OPTARG requires an argument";
             usage
+            die "option -$OPTARG requires an argument";
             ;;
          
         h | *)
@@ -56,11 +59,17 @@ if [[ $(findmnt -M "$MOUNT_DIR") ]] ; then
 fi
 
 [[ "$EUID" -ne 0 ]] && die "Please run this script as root!"
-[[ -z $ISO_IN ]] && die "Specify source iso: -i <path/to/source.iso>"
+[[ -z $ISO_IN ]] && usage && die "Specify source iso: -i <path/to/source.iso>"
 [[ -f $ISO_IN ]] || die "Source iso not found: $ISO_IN"
-[[ -z $ISO_OUT ]] && die "Specify iso destination path!"
-[[ -f $GRUB_CFG ]] || die "Cannot find grub cfg: $GRUB_CFG"
+
+[[ -z $ISO_OUT ]] && usage && die "Specify iso destination path!"
+[[ -f $ISO_OUT ]] && die "Iso destination path already exists at $ISO_OUT, remove first!"
+
+[[ -z $CFG_DIR ]] && usage && die "Specify path to config directory!"
+[[ -d $CFG_DIR ]] || die "Failed to find config directory, $CFG_DIR"
+
 [[ $(command -v mkisofs 2>&1) ]] || die "mkisofs not found, install first!"
+
 
 if [[ -d $TMP_DIR ]] ; then
     log "Removing tmp directory: $TMP_DIR..."
@@ -77,7 +86,7 @@ if (! cp -rp $MOUNT_DIR $TMP_DIR) ; then
 fi
 
 log "Copying grub.cfg"
-if (! cp -v $GRUB_CFG $TMP_DIR/boot/grub) ; then
+if (! cp -v $CFG_DIR/grub.cfg $TMP_DIR/boot/grub) ; then
     die "Failed to copy $GRUB_CFG to $TMP_DIR/boot/grub"
 fi
 
